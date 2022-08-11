b5b8e191ef0a9da01a4f80f342ab05242a3f808b
akozlova <annakozlova92@gmail.com>
2022-08-10 21:34:08 -0700

    add traverse llist function, add starting code for pop function

--- hb_recup/llist_singly.py
+++ hb_recup/llist_singly.py
@@ -30,6 +30,19 @@ class LinkedList:
         self.tail = new_node
         self.length = self.length + 1
 
+    def traverse(self):
+        """print out all data in each node"""
+        while(self.head):
+            print(self.head.data)
+            self.head = self.head.next
+
+    # TODO: finish this funciton
+    def pop(self):
+        """delete node from the end of linked list"""
+        while(self.head):
+            if self.head == None:
+                return None
+
 
 node1 = Node(10)
 node2 = Node(20)
@@ -41,8 +54,9 @@ new_llist.append(node1)
 new_llist.append(node2)
 new_llist.append(node3)
 
+new_llist.traverse()
 # can access only at the tail or head, or traverse trhough each item
-print(new_llist.head)
-print(new_llist.head.next)
-print(new_llist.tail)
-print(new_llist.length)
+# print(new_llist.head)
+# print(new_llist.head.next)
+# print(new_llist.tail)
+# print(new_llist.length)
