ea3af5acbe2cdb1597706f0fa1261819f8a27030
akozlova <annakozlova92@gmail.com>
2023-01-07 23:52:35 -0800

    add new linear bag interface placeholder and its usage in useBag.py

---
+++ dsaWithPythonByRance/dateADT/useBag.py
@@ -0,0 +1,11 @@
+myBag = Bag()
+myBag.add(10)
+myBag.add(20)
+myBag.add(30)
+myBag.add(40)
+
+value = int( input("Guess a value contained in the bag.") )
+if value in myBag:
+    print( "The bag contains the value", value )
+else :
+    print( "The bag does not contain the value", value )
