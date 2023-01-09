f5ec76b2e8db03d295a6f6e0e12392a36d0a43ef
akozlova <annakozlova92@gmail.com>
2023-01-08 19:33:44 -0800

    add imports to use bag ADT and it's iterator

--- dsaWithPythonByRance/dateADT/useBag.py
+++ dsaWithPythonByRance/dateADT/useBag.py
@@ -1,10 +1,12 @@
-myBag = Bag()
+import linearbag
+
+myBag = linearbag.Bag()
 myBag.add(10)
 myBag.add(20)
 myBag.add(30)
 myBag.add(40)
 
-value = int( input("Guess a value contained in the bag.") )
+value = int( input("Guess a value contained in the bag: ") )
 if value in myBag:
     print( "The bag contains the value", value )
 else :
