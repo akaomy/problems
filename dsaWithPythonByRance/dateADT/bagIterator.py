0952b8cf892679278ba58e4c5f2ae4a7b2a8e8b0
akozlova <annakozlova92@gmail.com>
2023-01-08 19:31:41 -0800

    add functionality into the methods of bag iterator

--- dsaWithPythonByRance/dateADT/bagIterator.py
+++ dsaWithPythonByRance/dateADT/bagIterator.py
@@ -1,10 +1,16 @@
 class BagIterator:
 
-    def __init__(self):
-        pass
+    def __init__(self, _items):
+        self._bagItems = _items
+        self._currentItem = 0
 
     def __iter__(self):
-        pass
+        return self
 
     def __next__(self):
-        pass
+        if self._currentItem < len(self._bagItems):
+            item = self._bagItems[self._currentItem]
+            self._currentItem += 1
+            return item
+        else:
+            raise StopIteration
