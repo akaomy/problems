a0f15121684efab39ff446eb0997fec4481bc743
akozlova <annakozlova92@gmail.com>
2023-01-09 17:07:18 -0800

    print out items in myBag and initial values for Bag

--- dsaWithPythonByRance/dateADT/useBag.py
+++ dsaWithPythonByRance/dateADT/useBag.py
@@ -1,10 +1,14 @@
 import linearbag
 
 myBag = linearbag.Bag()
+m_lst = [10, 20, 30, 40]
 myBag.add(10)
 myBag.add(20)
 myBag.add(30)
 myBag.add(40)
+
+print(myBag.__iter__(m_lst).__dict__)
+print(myBag._items)
 
 value = int( input("Guess a value contained in the bag: ") )
 if value in myBag:
